6c736d1421576cea64c7e1b244da8e9c3e79d0e4
Bluepanda <100101643+bluepanda280@users.noreply.github.com>
2024-03-07 17:36:42 -0700

    Updated The .PY File

---
+++ Not A Scam.py
@@ -0,0 +1,60 @@
+
+import time
+import turtle
+
+print("-DEFINITELY NOT A SCAM-")
+print()
+myName = input("Your Name: ")
+print("")
+print("Thanks for logging in " + myName)
+print("")
+cardNumber = input("What is your credit card number?: ")
+print("")
+print("Thanks " + myName + " I definitely wont put " + cardNumber +  " Into Amazon and order anything weird...")
+print("")
+print("Promise")
+print("")
+maidenName = input("What Is The Answer To Your Security Question?: ")
+print()
+print("That's cool, I just wanted to know that " + maidenName)
+print("was your Answer. Not because the bank requested it or anything, honest.")
+print()
+print()
+turtle.clearscreen
+time.sleep(3)
+print()
+print()
+print("------------------------------------")
+print("!New Notification!")
+print("Thank You For Buying 1 'candy'")
+print("------------------------------------")
+print()
+a = input("Would You Like To Cancel The Order (y/n)")
+if a == ("y"):
+    print("Error, Marked As no")
+elif a == ("n"):
+    print(" Ok")
+else:
+    print("Error, Marked As No")
+    
+time.sleep(3)
+    
+turtle.clearscreen
+print(" Order Confirmed")
+
+time.sleep(3)
+turtle.clearscreen
+
+print("")
+print("")
+print("-------------------------------------------")
+print("Thanks For Playing" + myName)
+print("DATA COLECTED")
+print("Name: " + myName)
+print("Credit Card: " + cardNumber)
+print("Secerity Question: " + maidenName)
+print("y/n answer: " + a)
+print("-------------------------------------------")
+print("Closing In 10 Seconds")
+time.sleep(10)
+
